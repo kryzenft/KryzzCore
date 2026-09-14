a27e966d7f155ce8132b185b9bc4891aaafd0c91
kryzenft <ftkryzen@gmail.com>
2026-09-14 18:50:29 +0530

    First

---
+++ src/main/java/dev/kryzen/kryzzCore/KryzzCore.java
@@ -0,0 +1,16 @@
+package dev.kryzen.kryzzCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KryzzCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
